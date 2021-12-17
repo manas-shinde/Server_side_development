be7ca15c30f5064026c6eeaf62cf3152dfbcf303
manas-shinde <93145391+manas-shinde@users.noreply.github.com>
2021-12-17 09:20:26 -0800

    Add files via upload

--- curd_operation_on_rest_api/routes/user.js
+++ curd_operation_on_rest_api/routes/user.js
@@ -1,1 +1,37 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const userRouter = express.Router();
+const uuid_v4 = require("uuid-v4");
 
+const { getAllUsers,createUser,updateUserWithoutId,deleteAllUsers ,getUserById,updateUserById,updateUserbyId,deleteUserById} = require("../controllers/user.js");
+
+
+// const users = require("../controllers/user.js");
+
+userRouter.use(bodyParser.json());
+
+
+userRouter.route("/")
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/json');//We are Sending JSON data as a response 
+        next();
+    })
+    .get(getAllUsers)
+    .post(createUser)
+    .put(updateUserWithoutId)
+    .delete(deleteAllUsers)
+
+userRouter.route("/:id")
+    .all((req, res, next) => {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/json");
+        next();
+    })
+    .get(getUserById)
+    .put(updateUserById)
+    .patch(updateUserbyId)
+    .delete(deleteUserById)
+
+module.exports = userRouter;
+     
